2459827d8aac752b528b865c697ba120176fa140
Anton Shurpin <ashurpin@ozon.ru>
2023-03-20 00:55:29 +0530

    Enhance migrate command

--- cmd/migration.go
+++ cmd/migration.go
@@ -13,6 +13,11 @@ import (
 )
 
 func init() {
+	migrationCmd.AddCommand(migrationUpCmd)
+	migrationCmd.AddCommand(migrationUpByOneCmd)
+	migrationCmd.AddCommand(migrationVersionCmd)
+	migrationCmd.AddCommand(migrationStatusCmd)
+
 	rootCmd.AddCommand(migrationCmd)
 }
 
@@ -20,32 +25,109 @@ var migrationCmd = &cobra.Command{
 	Use:   "migration",
 	Short: "Database migrations",
 	Long:  `Migrate database`,
+}
+
+func prepareGoose() error {
+	goose.SetBaseFS(migrations.FS)
+
+	return goose.SetDialect("postgres")
+}
+
+func prepareDB(ctx context.Context) (*sql.DB, error) {
+	var cfg config.Config
+	app := fx.New(
+		fxapp.Module,
+		fx.Populate(&cfg),
+	)
+
+	if err := app.Start(ctx); err != nil {
+		return nil, err
+	}
+	defer app.Stop(ctx)
+
+	db, err := sql.Open("pgx", cfg.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func dieOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustPrepareGooseAndDB(ctx context.Context) *sql.DB {
+	db, err := prepareDB(ctx)
+	dieOnError(err)
+
+	err = prepareGoose()
+	dieOnError(err)
+	return db
+}
+
+var migrationUpCmd = &cobra.Command{
+	Use:   "up",
+	Short: "Database migrations",
+	Long:  `Migrate database`,
 	Run: func(cmd *cobra.Command, args []string) {
-		var cfg config.Config
-		app := fx.New(
-			fxapp.Module,
-			fx.Populate(&cfg),
-		)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-		if err := app.Start(context.Background()); err != nil {
-			panic(err)
-		}
-		defer app.Stop(context.Background())
+		var err error
+		db := mustPrepareGooseAndDB(ctx)
 
-		goose.SetBaseFS(migrations.FS)
+		err = goose.Up(db, ".")
+		dieOnError(err)
+	},
+}
 
-		if err := goose.SetDialect("postgres"); err != nil {
-			panic(err)
-		}
+var migrationUpByOneCmd = &cobra.Command{
+	Use:   "upbyone",
+	Short: "Database migrations",
+	Long:  `Migrate database`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-		// setup database
-		db, err := sql.Open("pgx", cfg.DSN)
-		if err != nil {
-			panic(err)
-		}
+		var err error
+		db := mustPrepareGooseAndDB(ctx)
 
-		if err := goose.Up(db, "."); err != nil {
-			panic(err)
-		}
+		err = goose.UpByOne(db, ".")
+		dieOnError(err)
+	},
+}
+
+var migrationVersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Database migrations",
+	Long:  `Migrate database`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var err error
+		db := mustPrepareGooseAndDB(ctx)
+
+		err = goose.Version(db, ".")
+		dieOnError(err)
+	},
+}
+
+var migrationStatusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Database migrations",
+	Long:  `Migrate database`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var err error
+		db := mustPrepareGooseAndDB(ctx)
+
+		err = goose.Status(db, ".")
+		dieOnError(err)
 	},
 }
